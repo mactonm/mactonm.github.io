8e2d7b56fa4ddcbba7ca9625feaa1293451b97ad
mactonm <90670782+mactonm@users.noreply.github.com>
2026-02-14 02:18:14 +0000

    Deploying to gh-pages from @ mactonm/mactonm.github.io@bedfc68a6dc9ce0f636b0a73b874d1cccf7729df 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,10 +18,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/_pages/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
