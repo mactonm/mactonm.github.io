c00bdee8bc458d0ed35ddffd9718e41c396a9004
mactonm <90670782+mactonm@users.noreply.github.com>
2026-02-14 00:00:17 +0000

    Deploying to gh-pages from @ mactonm/mactonm.github.io@58684856118c9aa40fb6c5c9cc46ab0500bdd462 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -399,8 +399,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-our-paper-learning-from-scarcity-building-and-benchmarking-speech-technology-for-sukuma-accepted-to-the-eacl-2026-workshop-on-language-models-for-low-resource-languages-smile",
-          title: 'Our paper Learning from Scarcity: Building and Benchmarking Speech Technology for Sukuma. accepted...',
+            },},{id: "news-new-paper-our-work-learning-from-scarcity-building-and-benchmarking-speech-technology-for-sukuma-has-been-accepted-to-the-eacl-2026-workshop-on-language-models-for-low-resource-languages-loreslm-sparkles-smile",
+          title: 'New Paper! 📚 Our work, “Learning from Scarcity: Building and Benchmarking Speech Technology...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
