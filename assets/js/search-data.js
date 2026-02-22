cee93a6da6636136dd9c107cde333fe5af4ec72e
mactonm <90670782+mactonm@users.noreply.github.com>
2026-02-22 01:21:16 +0000

    Deploying to gh-pages from @ mactonm/mactonm.github.io@d48a6f4d91bb5abbae0a5998e54dfee4244d8229 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-about-me",
+    title: "About me",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/cv/";
+            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
